fb66144619c111c1ace4afc320a866fcef76bee1
Grivar <zodiak.miron@gmail.com>
2025-05-17 13:39:41 +0200

    The Bgame.exe for Windows

--- test.c
+++ test.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	srand(time(NULL));
 	char choice;
-	
+
 	printf("\033[34mWelcome to GUESS WHY I'M MAD simulator! 1.0\033[0m\n");
 
 	do{
@@ -28,7 +28,7 @@ int main(void)
 	return 0;
 }		
 
-
+	// The game fm.
 	void Bgame(void){
 
 	system("cls");
@@ -36,20 +36,22 @@ int main(void)
 	int rn_nb[] = {1,2,3,4,5};
 	int size = sizeof(rn_nb) / sizeof(rn_nb[0]);
 
+	// God knows my eyes hurts just from looking at this madness
+
 	const char *rn_wr[] = {"*She frowns at you*", "\"What do you mean?\"", "\"How was you day?\"", "\"I want a divorce\"",
-"\"I want kids\"", "*she is crying*"};
+"\"I want kids\"", "*she is crying*", "\"Did you apply for any jobs today?\"", "\"I think i'm pregnant\"", ""};
 	char Wsize = sizeof(rn_wr) / sizeof(rn_wr[0]);
 	int Wordw = (rand() % Wsize);
 
-	const char *rn_1wr[] = {"*give her flowers*", "\"Are you ok?\"", "\"Love you\"", "I brought food"};
+	const char *rn_1wr[] = {"*give her flowers*", "\"Are you ok?\"", "\"Love you\"", "I brought food", "*Give her a hug*", "\"I got the job!\"", "\"Will you marry me?\""};
 	char W1size = sizeof(rn_1wr) / sizeof(rn_1wr[0]);
 	int Word1 = (rand() % W1size);
 	
-	const char *rn_2wr[] = {"\"meow\"", "*yawn*", "\"Just chill\"", "\"Red calender again huh!?\""};
+	const char *rn_2wr[] = {"\"meow\"", "*yawn*", "\"Just chill\"", "\"Red calender again huh!?\"", "\"I took second mortgage\"", "*do the thing*"};
 	char W2size = sizeof(rn_2wr) / sizeof(rn_2wr[0]);
 	int Word2 = (rand() % W2size);
 
-	const char *rn_3wr[] = {"*ignore her*", "\"I want a divorce\"", "\"Come on! It was just a cat.\"", "\"You kind of look like Colette from Ratatouille\""};
+	const char *rn_3wr[] = {"*ignore her*", "\"I want a divorce\"", "\"Come on! It was just a cat.\"", "\"You kind of look like Colette from Ratatouille\"", "\"Do you know this guy in the closet?\"", "*do a backflip*"};
 	char W3size = sizeof(rn_3wr) / sizeof(rn_3wr[0]);
 	int Word3 = (rand() % W3size);
 
